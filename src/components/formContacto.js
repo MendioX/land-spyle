a4e8c492a222ba3b159f6fa0d94b2eb3e8aa541b
MendioX <mendiolacolombonahueltm6@gmail.com>
2025-03-18 13:10:25 -0300

    fix form

--- src/components/formContacto.js
+++ src/components/formContacto.js
@@ -38,7 +38,7 @@ export const Contacto = () =>  {
         }
     
         try {
-          const response = await fetch("http://localhost:5000/api/send-email", {
+          const response = await fetch(process.env.REACT_APP_BACKEND_URL+"/api/send-email", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
